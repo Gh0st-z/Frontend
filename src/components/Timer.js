e6c3f7f6e2d760c1a3d301f27df92737943b9c97
Gh0st-z <shinigamikami446@gmail.com>
2023-11-08 12:02:18 +0545

    Added firebase notification system

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,59 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+import { requestForToken, onMessageListener } from './firebase';
 
-const Timer = () => {
-  const initialSeconds = 60; // Initial timer duration
-  const [seconds, setSeconds] = useState(initialSeconds);
-  const [timerId, setTimerId] = useState(null);
+const Notification = () => {
+  const [notification, setNotification] = useState({title: '', body: ''});
+  const notify = () =>  toast(<ToastDisplay/>);
+  function ToastDisplay() {
+    return (
+      <div>
+        <p><b>{notification?.title}</b></p>
+        <p>{notification?.body}</p>
+      </div>
+    );
+  };
 
   useEffect(() => {
-    if (seconds === 0) {
-      clearTimeout(timerId);
-      sendPushNotification();
+    if (notification?.title ){
+     notify()
     }
-  }, [seconds, timerId]);
+  }, [notification])
 
-  const startTimer = () => {
-    if (seconds > 0) {
-      const id = setInterval(() => {
-        setSeconds(prevSeconds => prevSeconds - 1);
-      }, 1000);
+  requestForToken();
 
-      setTimerId(id);
-
-      // Request notification permission
-      if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
-        Notification.requestPermission()
-          .then(permission => {
-            if (permission === 'granted') {
-              console.log('Notification permission granted');
-            }
-          });
-      }
-    }
-  };
-
-  const resetTimer = () => {
-    clearTimeout(timerId);
-    setSeconds(initialSeconds);
-  };
-
-  const sendPushNotification = () => {
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      navigator.serviceWorker.ready.then(registration => {
-        registration.showNotification('Timer Ended', {
-          body: 'Your timer has ended.',
-        });
-      });
-    }
-  };
+  onMessageListener()
+    .then((payload) => {
+      setNotification({title: payload?.notification?.title, body: payload?.notification?.body});     
+    })
+    .catch((err) => console.log('failed: ', err));
 
   return (
-    <div>
-      <p>Timer: {seconds} seconds</p>
-      <button onClick={startTimer}>Start Timer</button>
-      <button onClick={resetTimer}>Reset Timer</button>
-    </div>
-  );
-};
+     <Toaster/>
+  )
+}
 
-export default Timer;
+export default Notification
