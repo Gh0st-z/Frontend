ecb0bb7b45251cfd61ec898ad3fbac0a0df6b459
Gh0st-z <shinigamikami446@gmail.com>
2023-12-07 08:01:57 +0545

    images and login pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,18 @@
 import React, { useState } from 'react';
+import './Login.css'
 
 function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit} method="POST" encType="multipart/form-data">
-        <h1>Welcome Back!</h1>
-        <div className="wrap-input100">
-          <span className="label-input100">Username:</span>
-          <input
-            className="input100"
-            type="text"
-            name="username"
-            placeholder="Type your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="wrap-input100">
-          <span className="label-input100">Password:</span>
-          <input
-            className="input100"
-            type="password"
-            name="password"
-            placeholder="Type your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        {/* ... other form fields and buttons ... */}
-        <div className="social-icons">
-        </div>
-      </form>
+    <div class = "container">
+      <div class = "login-form">
+        <form action="" method="POST" enctype="multipart/form-data">
+          <h1 style={{margin: 25 + 'px'}}>Welcome Back!</h1>
+          <div class="wrap-input100">
+            <span class="label-input100">Username: </span>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
