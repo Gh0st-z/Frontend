8fd67807ba154247ad841cb497cea93f354cf945
Gh0st-z <shinigamikami446@gmail.com>
2023-12-17 16:29:20 +0545

    Fixed axios connections and registration api (working now)

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {lazy, useEffect, useState} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './Register.css'
@@ -12,43 +12,72 @@ import '../static/vendor/select2/select2.min.css'
 import '../static/vendor/daterangepicker/daterangepicker.css'
 
 function Registerform(){
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({
+        first_name: '',
+        middle_name: '',
+        last_name: '',
+        email: '',
+        phone_number: '',
+        password: '',
+    });
 
+    const handleSubmit= (e) =>{
+        e.preventDefault();
+        axios.post('http://localhost:8000/autho/register/', formData)
+        .then(response => {
+            console.log(response.data.message);
+            setMessage(response.data.message);
+        }).catch(error =>{
+            console.log(error);
+            setMessage('Error occured during registration.');
+        });
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+      
     return(
     <div id = 'main_reg'>
       <div id = 'left-side'>
         <div class = "reg-form">
-            <form action="" method="POST" enctype="multipart/form-data">
+            <form action="" method="POST" enctype="multipart/form-data" onSubmit={handleSubmit}>
             <h1 id = "cacc">Create an account</h1>
             <div class="name-input100">
                 <span class="name-label-input100">First Name: </span>
-                <input class="n-input100" type="text" name="first_name"/>
+                <input class="n-input100" type="text" name="first_name" onChange={handleInputChange}/>
             </div>
             <div class="name-input100">
                 <span class="name-label-input100">Middle Name: </span>
-                <input class="n-input100" type="text" name="middle_name"/>
+                <input class="n-input100" type="text" name="middle_name" onChange={handleInputChange}/>
             </div>
             <div class="name-input100">
                 <span class="name-label-input100">Last Name: </span>
-                <input class="n-input100" type="text" name="last_name" />
+                <input class="n-input100" type="text" name="last_name" onChange={handleInputChange}/>
             </div>
             <div class="wrap-input100">
                 <span class="label-input100">Email: </span>
-                <input class="input100" type="email" name="email" placeholder="Enter your Email"/>
+                <input class="input100" type="email" name="email" placeholder="Enter your Email" onChange={handleInputChange}/>
                 <span class="focus-input100" data-symbol="&#xf206;"></span>
             </div>
             <div class="wrap-input100">
                 <span class="label-input100">Phone Number: </span>
-                <input class="input100" type="email" name="email" placeholder="Enter your Phone Number"/>
+                <input class="input100" type="text" name="phone_number" placeholder="Enter your Phone Number" onChange={handleInputChange}/>
                 <span class="focus-input100" data-symbol="&#9743;"></span>
             </div>
             <div class="wrap-input100">
                 <span class="label-input100">Password: </span>
-                <input class="input100" type="password" name="pass" placeholder="Enter your password"/>
+                <input class="input100" type="password" name="password" placeholder="Enter your password" onChange={handleInputChange}/>
                 <span class="focus-input100" data-symbol="&#xf190;"></span>
             </div>
             <div class="wrap-input100">
                 <span class="label-input100">Confirm Password: </span>
-                <input class="input100" type="password" name="pass" placeholder="Re-Enter your password"/>
+                <input class="input100" type="password" name="password2" placeholder="Re-Enter your password"/>
                 <span class="focus-input100" data-symbol="&#xf190;"></span>
             </div>
             <div class="caps-lock-warning">
