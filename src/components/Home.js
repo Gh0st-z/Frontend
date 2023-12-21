3943baa30fbd4f6801bd68313ec9fb957ef366f1
Gh0st-z <shinigamikami446@gmail.com>
2023-12-21 11:03:32 +0545

    Home css added with navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,15 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
+import '../static/css/Home.css'
+import '../static/vendor/bootstrap/css/bootstrap.min.css'
+import '../static/fonts/font-awesome-4.7.0/css/font-awesome.min.css'
+import '../static/fonts/iconic/css/material-design-iconic-font.min.css'
+import '../static/vendor/animate/animate.css'
+import '../static/vendor/css-hamburgers/hamburgers.min.css'
+import '../static/vendor/animsition/css/animsition.min.css'
+import '../static/vendor/select2/select2.min.css'
+import '../static/vendor/daterangepicker/daterangepicker.css'
 
 function HomePage(){
 
@@ -33,8 +42,11 @@ function HomePage(){
   }, []);
   
   return(
-    <div>
+    <div id="main-home">
       <ToastContainer/>
+      <div id="head-nav">
+        
+      </div>
     </div>
   );
 }
