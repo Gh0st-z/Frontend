8cf395e876220bb5413c66d5ea4e508a21c0be20
Gh0st-z <shinigamikami446@gmail.com>
2024-03-29 20:05:00 -0700

    Admin dashboard fix

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -51,42 +51,62 @@ function AdminDashboard(){
   };
 
   return(
-    <div class="dashboard">
-      <ToastContainer/>
-      <div class="left-navigation">
-        <div class="pharmacy-name">
-          Pharmacy Name
+    <div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar">
+				<div class="custom-menu">
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	          <i class="fa fa-bars"></i>
+	          <span class="sr-only">Toggle Menu</span>
+	        </button>
         </div>
-        <div class="dashboard-button">
-          <button class="nav-buttons"><Link class="button-links" to="/admin-dashboard">Dashboard</Link></button>
-        </div>
-        <div class="manage-staffs">
-          <button class="nav-buttons"><Link class="button-links" to="/manage-staffs">Manage Staffs</Link></button>
-        </div>
-        <div class="manage-users">
-          <button class="nav-buttons"><Link class="button-links" to="/manage-users">Manage Medicine</Link></button>
-        </div>
-        <div class="manage-products">
-          <button class="nav-buttons"><Link class="button-links" to="/manage-products">Manage Products</Link></button>
-        </div>
-        <div class="admin-profile">
-          <button class="nav-buttons"><Link class="button-links" to="">Admin Profile</Link></button>
-        </div>
-        <div class="logout">
-          <button class="nav-buttons"><Link class="button-links" to="">Logout</Link></button>
-        </div>
+				<div class="p-4">
+		  		<h1><a href="index.html" class="logo">Portfolic <span>Portfolio Agency</span></a></h1>
+	        <ul class="list-unstyled components mb-5">
+	          <li class="active">
+	            <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
+	          </li>
+	          <li>
+	              <a href="#"><span class="fa fa-user mr-3"></span> About</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-briefcase mr-3"></span> Works</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-sticky-note mr-3"></span> Blog</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-suitcase mr-3"></span> Gallery</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-cogs mr-3"></span> Services</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-paper-plane mr-3"></span> Contacts</a>
+	          </li>
+	        </ul>
+
+	        <div class="mb-5">
+						<h3 class="h6 mb-3">Subscribe for newsletter</h3>
+						<form action="#" class="subscribe-form">
+	            <div class="form-group d-flex">
+	            	<div class="icon"><span class="icon-paper-plane"></span></div>
+	              <input type="text" class="form-control" placeholder="Enter Email Address"/>
+	            </div>
+	          </form>
+					</div>
+
+	        <div class="footer">
+	        	<p>
+						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+						  </p>
+	        </div>
+
+	      </div>
+    	</nav>
+
+      <div id="content" class="p-4 p-md-5 pt-5">
       </div>
-      <div class="right-dashboard">
-        <div id="card">
-          <div id="card-header">
-            Today's Sales
-          </div>
-          <div id="card-body">
-            $10,945
-          </div>
-        </div>
-      </div>
-    </div>
+		</div>
   );
 }
 
